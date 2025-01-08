1bf294b11a8714359d24e5d4678b0e2e5b0ab636
Beatrix Victoria <144822592+beatrixvv@users.noreply.github.com>
2025-01-08 20:57:59 +0800

    Added more features
    - Can show hanzi and pinyin simultaneously
    - Show copy tooltip
    - Fullscreen feature
    - Accept image input

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const result = document.getElementById("result-text");
+const showHanzi = document.getElementById("show-hanzi");
 const copyButton = document.getElementById("copy-button");
+const copyTooltip = document.getElementById("copy-tooltip");
+const fullScreen = document.getElementById("full-screen");
 const original = document.getElementById("original-text");
+const inputImg = document.getElementById("input-img");
 const clearButton = document.getElementById("clear-button");
 
 const convertionDict = {
@@ -25,24 +29,69 @@ const toneNum = Object.values(convertionDict)[0].length;
 
 // Display the converted pinyin
 // In first init
-result.innerText = convert(original.value);
+convert(original.value, result);
 // When input change
 original.addEventListener("input", (e) => {
-  result.innerText = convert(original.value);
+  convert(original.value, result);
 });
 
 // Remove default text
-original.addEventListener(
-  "click",
-  (e) => {
-    clear();
-  },
-  { once: true }
-);
+original.addEventListener("click", clear, { once: true });
+
+// Show hanzi
+showHanzi.addEventListener("change", (e) => {
+  convert(original.value, result);
+});
 
 // Copy text
 copyButton.addEventListener("click", (e) => {
   navigator.clipboard.writeText(result.innerText);
+  // Show tooltip
+  copyTooltip.classList.add("visible");
+  // Hide the tooltip after 2 seconds
+  setTimeout(() => {
+    copyTooltip.classList.remove("visible");
+  }, 800);
+});
+
+// Full screen feature
+fullScreen.addEventListener("click", () => {
+  if (result.requestFullscreen) {
+    result.requestFullscreen(); // For most browsers
+  } else if (result.webkitRequestFullscreen) {
+    result.webkitRequestFullscreen(); // For Safari
+  } else if (result.msRequestFullscreen) {
+    result.msRequestFullscreen(); // For IE11
+  }
+});
+
+// OCR feature
+inputImg.addEventListener("change", (e) => {
+  const file = inputImg.files[0];
+  if (!file) return;
+  if (file.type.startsWith("image/")) {
+    // Remove click event that remove the default text
+    original.removeEventListener("click", clear, { once: true });
+    // Read the image
+    const reader = new FileReader();
+    reader.onload = async function (e) {
+      const imageUrl = e.target.result;
+      original.value = "Processing file...";
+
+      // OCR
+      const worker = await Tesseract.createWorker("chi_sim");
+      const ret = await worker.recognize(imageUrl);
+      original.value = ret.data.text.replace(/[^\S\r\n]+/g, "");
+      await worker.terminate();
+      convert(original.value, result);
+      inputImg.value = "";
+    };
+    reader.readAsDataURL(file);
+  } else {
+    original.value = "Image only!";
+    inputImg.value = "";
+    result.innerText = "";
+  }
 });
 
 // Clear text
@@ -50,20 +99,59 @@ clearButton.addEventListener("click", (e) => {
   clear();
 });
 
-function convert(original) {
-  let result = [];
+function convertPinyin(original, result) {
+  let resultArr = [];
   const lines = original.split("\n");
   const regexHanzi = /[\u4e00-\u9fff]/;
 
   lines.forEach((line) => {
     if (regexHanzi.test(line)) {
-      result.push(hanziToPinyin(line));
+      resultArr.push(hanziToPinyin(line));
     } else {
-      result.push(pinyinToPinyin(line));
+      resultArr.push(pinyinToPinyin(line));
     }
   });
+  result.innerText = resultArr.join("\n");
+}
 
-  return result.join("\n");
+function convertHanziPinyin(original, result) {
+  result.innerHTML = "";
+  const lines = original.split("\n");
+  const regexHanzi = /[\u4e00-\u9fff]/;
+
+  lines.forEach((line) => {
+    let divLine = document.createElement("div");
+    divLine.className = "result-line";
+    if (regexHanzi.test(line)) {
+      wordArr = line.split("");
+      wordArr.forEach((word) => {
+        // WordContainer will encapsulate hanzi and pinyin
+        let wordContainer = document.createElement("div");
+        wordContainer.className = "word-container";
+        let hanzi = document.createElement("div");
+        hanzi.className = "hanzi";
+        hanzi.innerText = word;
+        let pinyin = document.createElement("div");
+        pinyin.className = "pinyin";
+        pinyin.innerText = hanziToPinyin(word);
+
+        wordContainer.appendChild(hanzi);
+        wordContainer.appendChild(pinyin);
+        divLine.appendChild(wordContainer);
+      });
+    } else {
+      divLine.innerText = pinyinToPinyin(line);
+    }
+    result.appendChild(divLine);
+  });
+}
+
+function convert(original, result) {
+  if (showHanzi.checked) {
+    return convertHanziPinyin(original, result);
+  } else {
+    return convertPinyin(original, result);
+  }
 }
 
 function hanziToPinyin(line) {
